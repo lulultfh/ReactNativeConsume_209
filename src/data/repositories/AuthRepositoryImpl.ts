896be2094945c58d9cd18f003f87dad1f7d4070f
lulultfh <punyalulu05@gmail.com>
2026-06-05 15:19:42 +0700

    feat: menambahkan implementasi untuk auth repo

---
+++ src/data/repositories/AuthRepositoryImpl.ts
@@ -0,0 +1,26 @@
+import { LoginResponse, RegisterResponse } from "@/domain/entities/Auth";
+import { IAuthRepository } from "@/domain/repositories/IAuthRepository";
+import apiClient from "../api/apiClient";
+
+export class AuthRepositoryImpl implements IAuthRepository {
+  async login(email: string, password: string): Promise<LoginResponse> {
+    const response = await apiClient.post<LoginResponse>("/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  }
+
+  async register(
+    username: string,
+    email: string,
+    password: string,
+  ): Promise<RegisterResponse> {
+    const response = await apiClient.post<RegisterResponse>("/auth/register", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  }
+}
