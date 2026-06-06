320b75b0c058f36213b18f4a075ecea620f632eb
lulultfh <punyalulu05@gmail.com>
2026-06-07 01:32:36 +0700

    feat: menambahkan halaman edit hewan yang mengintegrasikan viewmodel dan merender component formHewan

--- src/app/main/edit/[id].tsx
+++ src/app/main/edit/[id].tsx
@@ -1,1 +1,49 @@
-export default function EditHewanScreen() {}
+import FormHewan from "@/components/FormHewan";
+import { Hewan } from "@/domain/entities/Hewan";
+import { useHewanViewModel } from "@/hooks/useHewanViewModel";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+export default function EditHewanScreen() {
+  const { id } = useLocalSearchParams();
+  const { loading, updateHewan, fetchHewanById } = useHewanViewModel();
+  const [hewan, setHewan] = useState<Hewan | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const res = await fetchHewanById(Number(id));
+    if (res?.success) {
+      setHewan(res.data);
+    }
+  };
+  if (!hewan) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  return (
+    <FormHewan
+      title="Edit Data Ternak"
+      submit="Update Data"
+      initialData={hewan}
+      loading={loading}
+      onSubmit={(data) => {
+        updateHewan(Number(id), data, () => {
+          router.back();
+        });
+      }}
+    />
+  );
+}
