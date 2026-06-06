69140198584776bfb04f2f8a65ae7d7ba4b099d3
lulultfh <punyalulu05@gmail.com>
2026-06-07 00:40:24 +0700

    feat: menambahkan initialData, loading, submit, dan onSubmit pada HewanProps

--- src/components/FormHewan.tsx
+++ src/components/FormHewan.tsx
@@ -1,1 +1,10 @@
+import { Hewan } from "@/domain/entities/Hewan";
+
+interface HewanProps {
+  initialData?: Hewan;
+  loading?: boolean;
+  submit?: string;
+  onSubmit: (data: Omit<Hewan, "id">) => void;
+}
+
 export default function FormHewan() {}
